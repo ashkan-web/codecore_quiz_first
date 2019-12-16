3869e314b7fecb60e299bbfc7d56c9d7cfd3e0e8
ashkan <ashkanh.info@gmail.com>
2019-12-16 10:19:14 -0800

    git dependencies was setuped

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,7 +7,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 

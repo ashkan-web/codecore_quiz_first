7d8e9fa3681c84649c4f95196765e7657fd1ce12
ashkan <ashkanh.info@gmail.com>
2019-12-16 10:27:19 -0800

    Static assets middleware setup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,20 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const PORT = process.env.PORT || 3000
+const DOMAIN = 'localhost'
+app.listen(PORT, DOMAIN, () => {
+  console.log(`Listening at http://${DOMAIN}:${PORT} in ${app.get('env')} environment`)
+})
